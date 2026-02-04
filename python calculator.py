ae45845ddf0e15eedb75a198ad6b951e5d701711
hetbuilds <avaiyahet.kep@gmail.com>
2026-02-04 22:37:12 +0530

    area calculTION FUNCTION ADDED

--- python calculator.py
+++ python calculator.py
@@ -2,17 +2,17 @@
 
 
 while True:
-     op = input("Enter operation (+, -, *, / ,%, sq, sqroot) or 'quit' to quit: ")
+     op = input("Enter operation (+, -, *, / ,%, sq, sqroot, area) or 'quit' to quit: ")
 
      if op == 'quit':
             print("Exiting the calculator. Goodbye!")
             break
      
-     if op not in ['+', '-', '*', '/', '%', 'sq', 'sqroot']:
+     if op not in ['+', '-', '*', '/', '%', 'sq', 'sqroot', 'area']:
         print("Invalid operation. Please try again.")
         continue
         
-     if op not in ['sq', 'sqroot']:
+     if op not in ['sq', 'sqroot', 'area']:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
             if op == '+':
@@ -31,6 +31,30 @@ while True:
                      print("Error: Division by zero is not allowed.")
                      continue
                 result = num1 / num2 * 100
+
+     elif op == 'area':
+            
+          
+            shape = input("Enter shape (circle, rectangle, triangle): ")
+            if shape == 'circle':
+                radius = float(input("Enter radius: "))
+                import math
+                result = math.pi * radius ** 2
+                appresult = 22/7 * radius ** 2
+                print(f"The area of the circle using approximate value of pi (22/7) is: ~{appresult}")
+                print(f"The area of the circle using exact value of pi is: ~{result}")
+            elif shape == 'rectangle':
+                length = float(input("Enter length: "))
+                width = float(input("Enter width: "))
+                result = length * width
+            elif shape == 'triangle':
+                base = float(input("Enter base: "))
+                height = float(input("Enter height: "))
+                result = 0.5 * base * height
+            else:
+                print("Invalid shape. Please try again.")
+                continue 
+                    
      else:
             if op == 'sqroot':
                 import math
